79d87d692daf568bc4550e43ca57bcbb70c8c2c3
maeder <c.maeder@uni-bremen.de>
2020-02-17 17:21:36 +0100

    refactored FoldSet

--- src/Rcl/Reduce.hs
+++ src/Rcl/Reduce.hs
@@ -1,4 +1,4 @@
-module Rcl.Reduce (reduction) where
+module Rcl.Reduce (reduction, Vars, runReduce) where
 
 import Control.Applicative ((<|>))
 import Control.Exception (assert)
@@ -9,26 +9,29 @@ import Rcl.Type
 
 data FoldSet a = FoldSet
   { foldBin :: Set -> BinOp -> a -> a -> a
-  , foldUn :: Set -> UnOp -> a -> a }
+  , foldUn :: Set -> UnOp -> a -> a
+  , foldPrim :: Set -> a }
 
 mapSet :: FoldSet Set
 mapSet = FoldSet
   { foldBin = const BinOp
-  , foldUn = const UnOp }
+  , foldUn = const UnOp
+  , foldPrim = id }
 
 const2 :: a -> b -> c -> a
 const2 = const . const
 
-constSet :: (a -> a -> a) -> FoldSet a
-constSet f = FoldSet
+constSet :: (a -> a -> a) -> a -> FoldSet a
+constSet f a = FoldSet
   { foldBin = const2 f
-  , foldUn = const2 id }
+  , foldUn = const2 id
+  , foldPrim = const a }
 
-foldSet :: FoldSet a -> (Set -> a) -> Set -> a
-foldSet r f s = case s of
-  BinOp o s1 s2 -> foldBin r s o (foldSet r f s1) $ foldSet r f s2
-  UnOp o p -> foldUn r s o $ foldSet r f p
-  _ -> f s
+foldSet :: FoldSet a -> Set -> a
+foldSet r s = case s of
+  BinOp o s1 s2 -> foldBin r s o (foldSet r s1) $ foldSet r s2
+  UnOp o p -> foldUn r s o $ foldSet r p
+  _ -> foldPrim r s
 
 data FoldStmt a b = FoldStmt
   { foldBool :: Stmt -> BoolOp -> b -> b -> b
@@ -56,16 +59,16 @@ replAO :: Set -> Set
 replAO = foldSet mapSet
   { foldUn = \ _ o p -> case o of
       AO -> BinOp Minus p (UnOp OE p)
-      _ -> UnOp o p } id
+      _ -> UnOp o p }
 
 findSimpleOE :: Stmt -> Maybe Set
 findSimpleOE = foldStmt (constStmt (<|>) (<|>)) findOE
 
 findOE :: Set -> Maybe Set
-findOE = foldSet (constSet (<|>))
+findOE = foldSet (constSet (<|>) Nothing)
   { foldUn = \ s _ r -> r <|> case s of
       UnOp OE p -> Just p -- we omit the outer OE
-      _ -> Nothing } $ const Nothing
+      _ -> Nothing }
 
 replaceOE :: Set -> Set -> Stmt -> Stmt
 replaceOE e = foldStmt mapStmt . replOE e
@@ -74,7 +77,7 @@ replOE :: Set -> Set -> Set -> Set
 replOE e r = foldSet mapSet
   { foldUn = \ _ o p -> case o of
       OE | p == e -> r
-      _ -> UnOp o p } id
+      _ -> UnOp o p }
 
 type Vars = [(Int, Set)]
 
@@ -95,9 +98,10 @@ replaceVar :: Int -> Set -> Stmt -> Stmt
 replaceVar i = foldStmt mapStmt . replVar i
 
 replVar :: Int -> Set -> Set -> Set
-replVar i r = foldSet mapSet $ \ s -> case s of
-  Var j t | i == j -> assert (typeOfSet r == t) $ UnOp OE r
-  _ -> s
+replVar i r = foldSet mapSet
+  { foldPrim = \ s -> case s of
+    Var j t | i == j -> assert (typeOfSet r == t) $ UnOp OE r
+    _ -> s }
 
 replaceMinus :: Stmt -> Stmt
 replaceMinus = foldStmt mapStmt replMinus
@@ -106,7 +110,7 @@ replMinus :: Set -> Set
 replMinus = foldSet mapSet
   { foldBin = \ _ o s1 s2 -> case o of
       Minus -> assert (s2 == UnOp OE s1) $ UnOp AO s1
-      _ -> BinOp o s1 s2 } id
+      _ -> BinOp o s1 s2 }
 
 reduceAndReconstruct :: Stmt -> String
 reduceAndReconstruct s = let
