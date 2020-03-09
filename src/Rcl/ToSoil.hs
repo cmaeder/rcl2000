5ff58a9caeb89b673133b654775b161309acc69a
maeder <c.maeder@uni-bremen.de>
2020-03-09 16:39:02 +0100

    refactored

--- src/Rcl/ToSoil.hs
+++ src/Rcl/ToSoil.hs
@@ -4,6 +4,7 @@ module Rcl.ToSoil (toSoil) where
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
+import Rcl.Ast
 import Rcl.Data
 import Rcl.ToOcl (aggName)
 
@@ -14,32 +15,36 @@ toSoil m = unlines $ let
   ss = sessions m
   sl = Map.toList ss
   us = userSets m
-  in new "R" role (roles m)
-  ++ new "U" name (users m)
-  ++ new "OP" operation (operations m)
-  ++ new "OBJ" object (objects m)
-  ++ new "P" pStr ps
-  ++ map (\ p -> '!' : pStr p ++ ".op := " ++ operation (op p)) pl
-  ++ map (\ p -> '!' : pStr p ++ ".obj := " ++ object (obj p)) pl
-  ++ new "S" id (Map.keysSet ss)
-  ++ map (\ (i, s) -> '!' : i ++ ".user := " ++ name (user s)) sl
-  ++ map (\ (u, r) -> "!insert (" ++ name u ++ "," ++ role r
-          ++ ") into UA") (Set.toList $ ua m)
-  ++ map (\ (p, r) -> "!insert (" ++ pStr p ++ "," ++ role r
-          ++ ") into PA") (Set.toList $ pa m)
-  ++ map (\ (s, r) -> "!insert (" ++ role s ++ "," ++ role r
-          ++ ") into RH")
-    (concatMap (\ (r, l) -> map (r,) $ Set.toList l) . Map.toList $ rh m)
-  ++ map (\ (s, r) -> "!insert (" ++ s ++ "," ++ role r
-          ++ ") into SessionRoles")
-    (concatMap (\ (i, s) -> map (i,) . Set.toList $ activeRoles s) sl)
-  ++ map (\ s -> "!new " ++ s ++ "('" ++ s ++ "')")
-    (Map.keys us)
-  ++ map (\ (s, t, r) -> "!insert (" ++ s ++ "," ++ r
-          ++ ") into " ++ aggName t)
+  in new R role (roles m)
+  ++ new U name (users m)
+  ++ new OP operation (operations m)
+  ++ new OBJ object (objects m)
+  ++ new P pStr ps
+  ++ set "op" pStr (operation . op) pl
+  ++ set "obj" pStr (object . obj) pl
+  ++ new S id (Map.keysSet ss)
+  ++ set "user" fst (name . user . snd) sl
+  ++ insert "UA" name role (Set.toList $ ua m)
+  ++ insert "PA" pStr role (Set.toList $ pa m)
+  ++ insert "RH" role role (concatMap (\ (r, l) -> map (r,) $ Set.toList l)
+    . Map.toList $ rh m)
+  ++ insert "SessionRoles" id role (concatMap (\ (i, s) -> map (i,)
+    . Set.toList $ activeRoles s) sl)
+  ++ map (\ s -> mkNew s s) ("RBAC" : Map.keys us)
+  ++ map (\ (s, t, r) -> mkInsert s r $ aggName t)
     (concatMap (\ (s, (t, _, l)) -> map (s, t,) l) $ Map.toList us)
-  ++ ["!new RBAC('RBAC')"]
 
-new :: String -> (a -> String) -> Set.Set a -> [String]
-new b f =
-  map (\ a -> "!new " ++ b ++ "('" ++ f a ++ "')") . Set.toList
+mkNew :: String -> String -> String
+mkNew c n = "!new " ++ c ++ "('" ++ n ++ "')"
+
+new :: Base -> (a -> String) -> Set.Set a -> [String]
+new b f = map (mkNew (show b) . f) . Set.toList
+
+set :: String -> (a -> String) -> (a -> String) -> [a] -> [String]
+set a f g = map $ \ o -> '!' : f o ++ "." ++ a ++ " := " ++ g o
+
+mkInsert :: String -> String -> String -> String
+mkInsert a b r = "!insert (" ++ a ++ "," ++ b ++ ") into " ++ r
+
+insert :: String -> (a -> String) -> (b -> String) -> [(a, b)] -> [String]
+insert r f g = map $ \ (a, b) -> mkInsert (f a) (g b) r
