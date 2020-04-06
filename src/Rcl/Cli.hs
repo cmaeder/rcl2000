accf40519d68f47e54eb842103a420296fd0b235
maederc <c.maeder@uni-bremen.de>
2020-04-06 23:06:06 +0200

    added options for input files

--- src/Rcl/Cli.hs
+++ src/Rcl/Cli.hs
@@ -2,7 +2,7 @@ module Rcl.Cli (cli) where
 
 import Control.Monad (when)
 import Data.Char (toLower)
-import Data.List (find, isSuffixOf)
+import Data.List (find)
 import qualified Data.Map as Map (empty)
 import Data.Maybe (fromMaybe)
 import Rcl.Ast (UserTypes, Stmt)
@@ -18,23 +18,22 @@ import Rcl.ToOcl (ocl)
 import Rcl.ToSoil (toSoil)
 import Rcl.Type (typeErrors)
 import System.Console.GetOpt
-import System.FilePath ((</>), addExtension)
+import System.FilePath ((</>), replaceExtension)
 
 cli :: String -> [String] -> IO ()
 cli prN args = case getOpt Permute options args of
-      (os, n, []) -> let o = foldl (flip id) defaultOpts os in
+      (os, n, []) -> let o = foldl (flip id) dOpts os in
         if help o then putStrLn $
           usageInfo ("usage: " ++ prN ++ " [options] <file>*") options
         else let
-          rm = readModel
-            $ map (\ f -> dir o </> addExtension (f o) "txt")
+          rm = readModel $ map (optsFile o)
             [rhFile, uaFile, paFile, sessFile, setsFile]
           in case n of
         [] -> if null os then rm >>= evalInput [] . initModel else
           putStrLn "unexpected options without file arguments"
         _ -> do
           eith <- if onlyPrint o then return $ Right Map.empty else
-            if getTypes o then fmap Right . readTypes $ dir o </> typesFile o
+            if getTypes o then fmap Right . readTypes $ optsFile o typesFile
             else fmap Left rm
           mapM_ (processFile eith o) n
       (_, _, errs) -> mapM_ putStrLn errs
@@ -55,14 +54,38 @@ options =
       "report type checking errors"
     , Option "r" ["reduce"] (NoArg $ \ o -> o {reduce = True})
       "show reductions and check reconstruction"
-    , Option "t" ["types"] (NoArg $ \ o -> o {getTypes = True})
-      "read user defined types from file"
+    , Option "d" ["dir"]
+      (ReqArg (\ f o -> o {dir = f}) "<dir>")
+      $ "directory for input files, default: " ++ dir dOpts
+    , Option "x" ["ext"]
+      (ReqArg (\ f o -> o {ext = f}) "<ext>")
+      $ "extension for input files, default: " ++ ext dOpts
+    , Option "t" ["types"]
+      (OptArg (\ mt o -> let o1 = o {getTypes = True} in
+                  maybe o1 (\ t -> o1 {typesFile = t}) mt) "file")
+      $ "read user defined types from file, default: " ++ typesFile dOpts
+    , Option "R" ["Roles"]
+      (ReqArg (\ f o -> o {rhFile = f}) "<file>")
+      $ "file for roles and role hierarchy, default: " ++ rhFile dOpts
+    , Option "U" ["Users"]
+      (ReqArg (\ f o -> o {uaFile = f}) "<file>")
+      $ "file for users to roles assignment, default: " ++ uaFile dOpts
+    , Option "P" ["Perms"]
+      (ReqArg (\ f o -> o {paFile = f}) "<file>")
+      $ "file for permissions to roles assignment, default: " ++ paFile dOpts
+    , Option "S" ["Sessions"]
+      (ReqArg (\ f o -> o {sessFile = f}) "<file>")
+      $ "file for sessions with user and roles, default: " ++ sessFile dOpts
+    , Option "s" ["sets"]
+      (ReqArg (\ f o -> o {setsFile = f}) "<file>")
+      $ "file for user defined (conflict) sets, default: " ++ setsFile dOpts
     , Option "u" ["use-file"]
       (ReqArg (\ f o -> o {useFile = f, toOcl = True}) "<file>")
-      "include RBAC use file in output file"
+      $ "include RBAC use file in output file, default: " ++ useFile dOpts
     , Option "o" ["output-file"]
       (ReqArg (\ f o -> o {outFile = f, toOcl = True}) "<file>")
-      "write use (and soil) to .use (and .soil) output file(s)"
+      $ "write use (and soil) to .use (and .soil) output file(s), default: "
+      ++ outFile dOpts
     , Option "e" ["evaluate"] (NoArg $ \ o -> o {evaluate = True})
       "evaluate formulas"
     , Option "i" ["interactive"] (NoArg $ \ o -> o {prompt = True})
@@ -79,18 +102,19 @@ data Opts = Opts
   , toOcl :: Bool
   , prompt :: Bool
   , help :: Bool
-  , dir :: String
-  , typesFile :: String
-  , rhFile :: String
-  , uaFile :: String
-  , paFile :: String
-  , sessFile :: String
-  , setsFile :: String
-  , useFile :: String
-  , outFile :: String }
+  , dir :: FilePath
+  , ext :: String
+  , typesFile :: FilePath
+  , rhFile :: FilePath
+  , uaFile :: FilePath
+  , paFile :: FilePath
+  , sessFile :: FilePath
+  , setsFile :: FilePath
+  , useFile :: FilePath
+  , outFile :: FilePath }
 
-defaultOpts :: Opts
-defaultOpts = Opts
+dOpts :: Opts
+dOpts = Opts
   { parens = True
   , format = "Uni"
   , pprint = False
@@ -102,7 +126,8 @@ defaultOpts = Opts
   , prompt = False
   , help = False
   , dir = "examples"
-  , typesFile = "types.txt"
+  , ext = "txt"
+  , typesFile = "types"
   , rhFile = "rh"
   , uaFile = "ua"
   , paFile = "pa"
@@ -110,6 +135,9 @@ defaultOpts = Opts
   , setsFile = "sets"
   , useFile = "use/RBAC.use"
   , outFile = "use/test" }
+
+optsFile :: Opts -> (Opts -> FilePath) -> FilePath
+optsFile o sel = dir o </> replaceExtension (sel o) (ext o)
 
 onlyPrint :: Opts -> Bool
 onlyPrint o = not $ or [check o, reduce o, evaluate o, toOcl o, prompt o]
@@ -119,7 +147,7 @@ form o = let low = map toLower in
   Form (fromMaybe Uni $ find (\ f -> low (show f) == low (format o))
     [LaTeX, Ascii]) $ parens o
 
-processFile :: Either Model UserTypes -> Opts -> String -> IO ()
+processFile :: Either Model UserTypes -> Opts -> FilePath -> IO ()
 processFile eith o file = parseFromFile parser file >>= reportParse eith o
 
 reportParse :: Either Model UserTypes -> Opts -> Either ParseError [Stmt]
@@ -134,8 +162,7 @@ reportParse mus o eith = case eith of
         e = evaluate o
         i = prompt o
         out = outFile o
-        suf = ".use"
-        use = if suf `isSuffixOf` out then out else out ++ suf
+        use = replaceExtension out "use"
         us = either getUserTypes id mus
     when (p || onlyPrint o) . putStrLn . render $ pStmts (form o) ast
     when c . putStrLn $ typeErrors us ast
@@ -144,7 +171,7 @@ reportParse mus o eith = case eith of
       str <- readMyFile (useFile o)
       writeFile use $ str ++ ocl us ast
       case mus of
-        Left m -> writeFile (out ++ ".soil") $ toSoil m
+        Left m -> writeFile (replaceExtension out "soil") $ toSoil m
         _ -> putStrLn "no .soil file written for option -t"
     case mus of
       Left m -> do
