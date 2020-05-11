2352b3f3b7690528a35a83cc9c24ff602e1be82f
maederc <c.maeder@uni-bremen.de>
2020-05-11 17:18:30 +0200

    extended test

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,15 +10,15 @@ main :: IO ()
 main = do
   let e = "examples"
       d = "test" </> "expectedParseErrors"
-      b = "rcl2000"
+      b = cli "rcl2000"
   es <- getDirectoryContents e
   let rs = map (e </>) $ filter ((== ".rcl") . takeExtension) es
-  mapM_ (\ f -> cli b $ ["-f", show f] ++ rs) forms
-  mapM_ (\ f -> cli b $ ["-n", "-f", show f] ++ rs) forms
-  mapM_ (\ o -> cli b $ o : rs) ["-h", "-c", "-r", "-e"]
-  mapM_ (\ o -> cli b $ o : "-t" : rs) ["-c", "-r"]
-  cli b $ "-otest" : rs
-  cli b $ ["-t", "-o"] ++ rs
+  mapM_ (\ f -> b $ ["-f", show f] ++ rs) forms
+  mapM_ (\ f -> b $ ["-n", "-f", show f] ++ rs) forms
+  mapM_ (\ o -> b $ o : "-t" : rs) ["-c", "-r"]
+  b $ ["-t", "-o"] ++ rs
+  mapM_ (\ o -> b $ o : rs) ["-h", "-c", "-r", "-e"]
+  mapM_ (\ v -> b $ ("-v" ++ show v) : "-otest" : rs) [0 .. 2 :: Int]
   ts <- getDirectoryContents d
   fs <- filterM doesFileExist $ map (d </>) ts
-  cli b fs
+  b fs
