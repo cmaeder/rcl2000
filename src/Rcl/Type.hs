dcedc6f6be98cfae3e7aa98e3b1e66c6575802ed
maederc <c.maeder@uni-bremen.de>
2020-05-27 10:42:42 +0200

    almost finished typing

--- src/Rcl/Type.hs
+++ src/Rcl/Type.hs
@@ -32,57 +32,38 @@ report :: String -> State String ()
 report = modify . const
 
 ty :: UserTypes -> Stmt -> State String Stmt
-ty us s = case s of
-  BoolOp o s1 s2 -> do
-    r1 <- ty us s1
-    r2 <- ty us s2
-    pure $ BoolOp o r1 r2
-  CmpOp o e1 e2 -> do
-    t1 <- tyTerm us e1
-    t2 <- tyTerm us e2
-    let md t = report $ t ++ ": " ++ ppStmt s
-        ds es = do
-          let l = Set.size es
-          when (l == 0) $ md "wrongly typed"
-          when (l > 1) $ md "ambiguous statement"
-        r = CmpOp o t1 t2
+ty us = foldStmt FoldStmt
+  { foldBool = \ _ o s1 s2 -> do
+    r1 <- s1
+    BoolOp o r1 <$> s2
+  , foldCmp = \ _ o e1 e2 -> do
+    t1 <- e1
+    t2 <- e2
+    let r = CmpOp o t1 t2
+        md m = report $ m ++ ": " ++ ppStmt r
     case (t1, t2) of
-        (Term TheSet (UnOp (Typed ts1) s1), Term TheSet (UnOp (Typed ts2) s2))
-          -> do
-            let ts = case o of
-                  Elem -> Set.filter (ifSet (`Set.member` ts1) False) ts2
-                  _ | o `elem` [Eq, Ne] -> compatSetTys ts1 ts2
-                  _ -> Set.empty
-            if Set.size ts == 1 then do
-                let t = getUniqueType ts
-                    rs1 = Set.filter (if o == Elem then (== t) . SetOf
-                      else eqOrElem t) ts1
-                    rs2 = Set.filter
-                      (if o == Elem then (== t) else eqOrElem t) ts2
-                if Set.size rs1 == 1 && Set.size rs2 == 1 then do
-                    r1 <- disambig (getUniqueType rs1) s1
-                    r2 <- disambig (getUniqueType rs2) s2
-                    pure . CmpOp o (Term TheSet r1) $ Term TheSet r2
-                  else do
-                    ds rs1
-                    ds rs2
-                    pure r
-              else do
-                ds ts
-                pure r
-        (Term TheSet (UnOp (Typed ts1) s1), EmptySet) | o `elem` [Eq, Ne] ->
-          if Set.size ts1 == 1 then do
-              r1 <- disambig (getUniqueType ts1) s1
-              pure $ CmpOp o (Term TheSet r1) e2
-            else do
-              ds ts1
-              pure r
+        (Term TheSet s1, Term TheSet s2) | o `elem` [Eq, Ne, Elem] -> do
+            let ts1 = getType s1
+                ts2 = getType s2
+                ts = if o == Elem then
+                  Set.filter (ifSet (`Set.member` ts1) False) ts2
+                  else compatSetTys ts1 ts2
+                filt t = any (`Set.member` ts) [t, SetOf t]
+                filt1 t = if o == Elem then SetOf t `Set.member` ts else filt t
+                filt2 t = if o == Elem then t `Set.member` ts else filt t
+            b1 <- filterType True filt1 s1
+            b2 <- filterType True filt2 s2
+            when (null ts) $ md "wrongly typed relation"
+            pure . CmpOp o (Term TheSet b1) $ Term TheSet b2
+        (Term TheSet s1, EmptySet) | o `elem` [Eq, Ne] -> do
+          n <- filterType True (const True) s1
+          pure $ CmpOp o (Term TheSet n) t2
         (Term Card _, Term Card _) | o /= Elem -> pure r
         (Term Card _, Num _) | o /= Elem -> pure r
         (Num _, Term Card _) | o /= Elem -> pure r
         _ -> do
           md "wrong comparison"
-          pure r
+          pure r } (tyTerm us)
 
 tyTerm :: UserTypes -> Term -> State String Term
 tyTerm us t = case t of
@@ -119,11 +100,6 @@ eqOrElem t1 t2 = case t1 of
   SetOf s -> s == t2
   _ -> t1 == t2
 
-getUniqueType :: Set.Set SetType -> SetType
-getUniqueType ts = case Set.toList ts of
-  [t] -> t
-  _ -> error "getUniqueType"
-
 filterType :: Bool -> (SetType -> Bool) -> Set -> State String Set
 filterType b f s = let
   md m = report $ m ++ ": " ++ ppSet s
@@ -146,7 +122,7 @@ tySet :: UserTypes -> Set -> State String Set
 tySet us = let
   md t s = report $ t ++ ": " ++ ppSet s
   in foldSet FoldSet
-   { foldBin = \ _ o s1 s2 -> do
+   { foldBin = \ s o s1 s2 -> do
       a1 <- s1
       a2 <- s2
       case o of
@@ -156,12 +132,11 @@ tySet us = let
           pure . mkTypedSet (Set.singleton $ toSet OP)
             $ BinOp o b1 b2 -- R + U x OBJ -> 2^OP
         _ -> do
-          let m1 = getType a1
-              m2 = getType a2
-              ts = compatSetTys m1 m2
+          let ts = compatSetTys (getType a1) $ getType a2
               filt t = any (`Set.member` ts) [t, SetOf t]
           b1 <- filterType False filt a1
           b2 <- filterType False filt a2
+          when (null ts) $ md "wrongly typed set operation" s
           pure . mkTypedSet ts $ BinOp o b1 b2
   , foldUn = \ _ o s1 -> do
       a1 <- s1
@@ -197,8 +172,11 @@ getUntypedSet s = case s of
   _ -> s
 
 compatSetTys :: Set.Set SetType -> Set.Set SetType -> Set.Set SetType
-compatSetTys s1 s2 =
-  Set.unions [compatSetTysAux t1 t2 | t1 <- Set.toList s1, t2 <- Set.toList s2]
+compatSetTys s1 s2 = case Set.toList s1 of
+  [] -> s2
+  l1 -> case Set.toList s2 of
+    [] -> s1
+    l2 -> Set.unions [compatSetTysAux t1 t2 | t1 <- l1, t2 <- l2]
 
 compatSetTysAux :: SetType -> SetType -> Set.Set SetType
 compatSetTysAux t1 t2 = case (t1, t2) of
