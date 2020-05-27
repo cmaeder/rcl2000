afad2852b2f1a476c2712d813f790cb6b00e27b0
maederc <c.maeder@uni-bremen.de>
2020-05-27 12:17:09 +0200

    almost identical type checking results

--- src/Rcl/Type.hs
+++ src/Rcl/Type.hs
@@ -21,10 +21,10 @@ mBaseType :: UserTypes -> Set -> Set.Set Base
 mBaseType us = Set.map baseType . typeOfSet us
 
 typeOfSet :: UserTypes -> Set -> Set.Set SetType
-typeOfSet us s = getType $ evalState (tySet us s) []
+typeOfSet us s = getType $ evalState (tySet us s) ""
 
 wellTyped :: UserTypes -> Stmt -> Maybe String
-wellTyped us s = case execState (ty us s) [] of
+wellTyped us s = case execState (ty us s) "" of
   "" -> Nothing
   e -> Just $ e ++ "\n  in: " ++ ppStmt s
 
@@ -40,7 +40,9 @@ ty us = foldStmt FoldStmt
     t1 <- e1
     t2 <- e2
     let r = CmpOp o t1 t2
-        md m = report $ m ++ ": " ++ ppStmt r
+        str = ppStmt r
+        md m = report $ m ++ ": " ++ str
+        ft = filterType ("comparison: " ++ str) True
     case (t1, t2) of
         (Term TheSet s1, Term TheSet s2) | o `elem` [Eq, Ne, Elem] -> do
             let ts1 = getType s1
@@ -51,12 +53,12 @@ ty us = foldStmt FoldStmt
                 filt t = any (`Set.member` ts) [t, SetOf t]
                 filt1 t = if o == Elem then SetOf t `Set.member` ts else filt t
                 filt2 t = if o == Elem then t `Set.member` ts else filt t
-            b1 <- filterType True filt1 s1
-            b2 <- filterType True filt2 s2
+            b1 <- ft filt1 s1
+            b2 <- ft filt2 s2
             when (null ts) $ md "wrongly typed relation"
             pure . CmpOp o (Term TheSet b1) $ Term TheSet b2
         (Term TheSet s1, EmptySet) | o `elem` [Eq, Ne] -> do
-          n <- filterType True (const True) s1
+          n <- ft (const True) s1
           pure $ CmpOp o (Term TheSet n) t2
         (Term Card _, Term Card _) | o /= Elem -> pure r
         (Term Card _, Num _) | o /= Elem -> pure r
@@ -71,7 +73,7 @@ tyTerm us t = case t of
     r <- tySet us s
     case b of
       Card -> do
-        n <- filterType True (const True) s
+        n <- filterType ("cardinality: " ++ ppSet s) True (const True) s
         pure $ Term b n
       TheSet -> pure $ Term b r
   _ -> pure t
@@ -79,15 +81,17 @@ tyTerm us t = case t of
 disambig :: SetType -> Set -> State String Set
 disambig t s = let
   rt = mkTypedSet (Set.singleton t)
-  r = rt s in case s of
+  r = rt s
+  ft = filterType ("set: " ++ ppSet s) True
+  in case s of
   BinOp o s1 s2 -> case o of
     Operations _ -> pure r
     _ -> do
-      m1 <- filterType True (eqOrElem t) s1
-      m2 <- filterType True (eqOrElem t) s2
+      m1 <- ft (eqOrElem t) s1
+      m2 <- ft (eqOrElem t) s2
       pure . rt $ BinOp o m1 m2
   UnOp o s1 -> if isOp o then pure r else do
-      r1 <- filterType True (\ t1 -> case o of
+      r1 <- ft (\ t1 -> case o of
         OE -> SetOf t == t1
         AO -> t1 == t
         Typed ts -> t1 == t && Set.member t ts
@@ -100,22 +104,22 @@ eqOrElem t1 t2 = case t1 of
   SetOf s -> s == t2
   _ -> t1 == t2
 
-filterType :: Bool -> (SetType -> Bool) -> Set -> State String Set
-filterType b f s = let
-  md m = report $ m ++ ": " ++ ppSet s
+filterType :: String -> Bool -> (SetType -> Bool) -> Set -> State String Set
+filterType str b f s = let
+  md m = report $ m ++ " " ++ str
   t = getType s
   l = Set.toList t
   r = filter f l
   in case r of
   [] -> do
-    unless (null l) $ md "wrongly typed set"
-    pure s
+    unless (null l) $ md "wrongly typed"
+    pure $ getUntypedSet s
   [a] -> case l of
     _ : _ : _ -> disambig a $ getUntypedSet s
     _ -> pure s
   _ -> do
     let rs = Set.fromList r
-    when b . md $ "ambiguous set (" ++ ppType rs ++ ")"
+    when b . md $ "ambiguous (" ++ ppType rs ++ ")"
     pure . mkTypedSet rs $ getUntypedSet s
 
 tySet :: UserTypes -> Set -> State String Set
@@ -127,28 +131,28 @@ tySet us = let
       a2 <- s2
       case o of
         Operations _ -> do
-          b1 <- filterType True (\ t -> any (`isElemOrSet` t) [R, U]) a1
-          b2 <- filterType True (isElemOrSet OBJ) a2
+          b1 <- filterType ("1st arg: " ++ ppSet s)
+            True (\ t -> any (`isElemOrSet` t) [R, U]) a1
+          b2 <- filterType ("2nd arg: " ++ ppSet s) True (isElemOrSet OBJ) a2
           pure . mkTypedSet (Set.singleton $ toSet OP)
             $ BinOp o b1 b2 -- R + U x OBJ -> 2^OP
         _ -> do
           let ts = compatSetTys (getType a1) $ getType a2
               filt t = any (`Set.member` ts) [t, SetOf t]
-          b1 <- filterType False filt a1
-          b2 <- filterType False filt a2
+          b1 <- filterType "" False filt a1
+          b2 <- filterType "" False filt a2
           when (null ts) $ md "wrongly typed set operation" s
           pure . mkTypedSet ts $ BinOp o b1 b2
-  , foldUn = \ _ o s1 -> do
+  , foldUn = \ s o s1 -> do
       a1 <- s1
-      b1 <- filterType (isOp o) (opArg o) a1
-      pure $ opResult o b1
+      b1 <- filterType ("application: " ++ ppSet s) (isOp o) (opArg o) a1
+      pure $ if Set.null $ getType b1 then UnOp o b1 else opResult o b1
   , foldPrim = \ s -> case s of
       PrimSet p -> do
         let ts = Map.findWithDefault Set.empty p us
         when (Set.null ts) $ md "unknown base set" s
         pure $ mkTypedSet ts s
-      _ -> pure s
-}
+      _ -> pure s }
 
 isOp :: UnOp -> Bool
 isOp o = case o of
